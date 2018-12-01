65197b55e1989b1537f4fe5e7f863df468cf6ec5
dpx <dingpx2015@gmail.com>
2018-12-01 17:47:32 +0800

    Finish Assignment1

--- assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -71,7 +71,7 @@ class KNearestNeighbor(object):
         # training point, and store the result in dists[i, j]. You should   #
         # not use a loop over dimension.                                    #
         #####################################################################
-        pass
+        dists[i][j] = np.sqrt(np.sum(np.square(self.X_train[j,:] - X[i,:])))
         #####################################################################
         #                       END OF YOUR CODE                            #
         #####################################################################
@@ -93,7 +93,7 @@ class KNearestNeighbor(object):
       # Compute the l2 distance between the ith test point and all training #
       # points, and store the result in dists[i, :].                        #
       #######################################################################
-      pass
+      dists[i,:] = np.sqrt(np.sum(np.square(self.X_train-X[i,:]),axis = 1)) 
       #######################################################################
       #                         END OF YOUR CODE                            #
       #######################################################################
@@ -121,7 +121,12 @@ class KNearestNeighbor(object):
     # HINT: Try to formulate the l2 distance using matrix multiplication    #
     #       and two broadcast sums.                                         #
     #########################################################################
-    pass
+    dists = np.multiply(np.dot(X,self.X_train.T),-2) 
+    sq1 = np.sum(np.square(X),axis=1,keepdims = True) 
+    sq2 = np.sum(np.square(self.X_train),axis=1) 
+    dists = np.add(dists,sq1) 
+    dists = np.add(dists,sq2) 
+    dists = np.sqrt(dists)
     #########################################################################
     #                         END OF YOUR CODE                              #
     #########################################################################
@@ -153,7 +158,7 @@ class KNearestNeighbor(object):
       # neighbors. Store these labels in closest_y.                           #
       # Hint: Look up the function numpy.argsort.                             #
       #########################################################################
-      pass
+      closest_y = self.y_train[np.argsort(dists[i,:])[:k]]
       #########################################################################
       # TODO:                                                                 #
       # Now that you have found the labels of the k nearest neighbors, you    #
@@ -161,7 +166,7 @@ class KNearestNeighbor(object):
       # Store this label in y_pred[i]. Break ties by choosing the smaller     #
       # label.                                                                #
       #########################################################################
-      pass
+      y_pred[i] = np.argmax(np.bincount(closest_y))
       #########################################################################
       #                           END OF YOUR CODE                            # 
       #########################################################################
--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -34,13 +34,17 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:, j] += X[i]
+        dW[:, y[i]] -= X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
-  loss += reg * np.sum(W * W)
+  loss += 0.5 * reg * np.sum(W * W)
+  dW += reg * W
 
   #############################################################################
   # TODO:                                                                     #
@@ -69,7 +73,19 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+
+  scores = X.dot(W)        # N by C
+  num_train = X.shape[0]
+  num_classes = W.shape[1]
+  scores_correct = scores[np.arange(num_train), y]   # 1 by N
+  scores_correct = np.reshape(scores_correct, (num_train, 1))  # N by 1
+  margins = scores - scores_correct + 1.0     # N by C
+  margins[np.arange(num_train), y] = 0.0
+  margins[margins <= 0] = 0.0
+  loss += np.sum(margins) / num_train
+  loss += 0.5 * reg * np.sum(W * W)
+
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -84,7 +100,11 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+    # compute the gradient
+  margins[margins > 0] = 1.0
+  row_sum = np.sum(margins, axis=1)                  # 1 by N
+  margins[np.arange(num_train), y] = -row_sum        
+  dW += np.dot(X.T, margins)/num_train + reg * W     # D by C
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
